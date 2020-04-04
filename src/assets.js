796dd1eab94cf92c1cbbad2c6cfc938697c04680
MarioDena <enrique.dn@outlook.com>
2020-04-03 22:28:59 -0600

    [Feature] finished layout

---
+++ src/assets.js
@@ -0,0 +1,4 @@
+import 'bootstrap';
+import './styles/bootstrap.css';
+import './styles/style.css';
+import Image1 from './assets/img1.jpg';
--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
-import './styles/style.css';
-import Image1 from './assets/img1.jpg';
+import assets from './assets.js';
 
-function component() {
-  const element = document.createElement('div');
+function createnavcontent(content, primary) {
+  const element = document.createElement('li');
+  element.classList.add('nav-item');
 
-  // Lodash, now imported by this script
-  element.innerHTML = 'Working';
-  element.classList.add('floating');
-  const myIcon = new Image();
-  myIcon.src = Image1;
+  const item = document.createElement('a');
+  item.href = '#';
+  // eslint-disable-next-line no-unused-expressions
+  !primary ? item.classList.add('nav-link') : item.classList.add('nav-link', 'active');
+  item.innerHTML = content;
 
-  element.appendChild(myIcon);
+  element.appendChild(item);
+
   return element;
 }
 
+function createnav() {
+  const element = document.createElement('ul');
+  element.classList.add('nav', 'nav-tabs', 'justify-content-end');
+  element.appendChild(createnavcontent('Home', true));
+  element.appendChild(createnavcontent('Menu', false));
+  element.appendChild(createnavcontent('Reservations', false));
 
-document.body.appendChild(component());
+  return element;
+}
+
+function contents() {
+  const element = document.createElement('div');
+
+  element.classList.add('content');
+  element.id = 'content';
+
+  element.appendChild(createnav());
+
+  // const myIcon = new Image();
+  // myIcon.src = Image1;
+
+  // element.appendChild(myIcon);
+
+  return element;
+}
+
+document.body.appendChild(contents());
